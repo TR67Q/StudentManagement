be71ced2a3984108e6add4cdc2efec791d1e572a
terakura <terakura312@gmail.com>
2024-08-24 19:46:50 +0900

    MySQLのstudents,students_coursesのテーブルの全件検索機能を追加

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -4,11 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -22,29 +18,13 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/students")
-	public List<Student> openAllStudents(){
-		return repository.openAllStudents();
+	@GetMapping("/studentList")
+	public List<Student> getStudentList(){
+		return repository.search();
 	}
 
-	@GetMapping("/student")
-	public String getStudent(@RequestParam String name){
-		Student student = repository.searchByName(name);
-		return student.getName() + " " + student.getAge() + "sai";
-	}
-
-	@PostMapping("/student")
-	public void registerStudent(String name, int age){
-		repository.registerStudent(name, age);
-	}
-
-	@PatchMapping("/student")
-	public void updateStudentName(String name, int age){
-		repository.updateStudent(name, age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
+	@GetMapping("/studentCourse")
+	public List<StudentCourse> getStudentCourseList(){
+		return repository.searchCourse();
 	}
 }
