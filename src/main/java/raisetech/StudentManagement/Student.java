bbab77c3f556203bebae8a4c1a5a712291cfec17
terakura <terakura312@gmail.com>
2024-08-25 16:47:59 +0900

    MySQLのstudents,students_coursesのテーブルの全件検索機能を追加

--- src/main/java/raisetech/StudentManagement/Student.java
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -1,23 +1,17 @@
 package raisetech.StudentManagement;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
-
+  private String id;
   private String name;
+  private String furigana;
+  private String nickname;
+  private String mailAddress;
+  private String area;
   private int age;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
+  private String gender;
 }
