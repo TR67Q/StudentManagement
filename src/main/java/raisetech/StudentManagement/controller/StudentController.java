a6d8265fb3fea09607a185c9c96d76fb768c5541
terakura <terakura312@gmail.com>
2024-10-12 14:15:22 +0900

    コードの整形とリファクタリング

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,53 +1,57 @@
 package raisetech.StudentManagement.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import raisetech.StudentManagement.controller.converter.StudentConverter;
-import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
+/**
+ * 受講生の検索や登録、更新などを行うRestAPIとして受け付けるController
+ */
 @RestController
 public class StudentController {
 
   private final StudentService service;
-  private final StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service, StudentConverter converter) {
+  public StudentController(StudentService service) {
     this.service = service;
-    this.converter = converter;
   }
 
+  /**
+   * 受講生一覧検索機能
+   * 全件検索を行うので、条件指定は行いません
+   *
+   * @return 受講生一覧（全件）
+   */
   @GetMapping("/studentList")
     public List<StudentDetail> getStudentList(){
-    List<Student> students = service.searchStudentList();
-    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
-    return converter.convertStudentDetails(students, studentsCourses);
+    return service.searchStudentList();
   }
 
-  @GetMapping("/newStudent")
-  public String newStudent(Model model){
-    StudentDetail studentDetail = new StudentDetail();
-    studentDetail.setStudentsCourses(Arrays.asList(new StudentsCourses()));
-    model.addAttribute("studentDetail", studentDetail);
-    return "registerStudent";
+  /**
+   * 受講生検索機能
+   * IDに紐づく任意の受講生情報を取得します
+   *
+   * @param id 受講生ID
+   * @return 受講生
+   */
+  @GetMapping("/student/{id}")
+  public StudentDetail getStudent(@PathVariable String id){
+    return service.searchStudent(id);
   }
 
   @PostMapping("/registerStudent")
-  public ResponseEntity<String> registerStudent(@RequestBody StudentDetail studentDetail, BindingResult result){
-    service.registerStudent(studentDetail);
-    return ResponseEntity.ok("新規登録処理が完了しました。");
+  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail, BindingResult result){
+    StudentDetail responseStudentDetail1 = service.registerStudent(studentDetail);
+    return ResponseEntity.ok(responseStudentDetail1);
   }
 
   @PostMapping("/updateStudent")
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -9,20 +9,45 @@ import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 
+/**
+ * 受講生テーブルと受講生コース情報テーブルに紐づくRepository
+ */
 @Mapper
 public interface StudentRepository {
 
+  /**
+   * 受講生の全件検索を行います
+   *
+   * @return 受講生一覧（全件）
+   */
   @Select("SELECT * FROM students")
   List<Student> search();
 
+  /**
+   * 受講生の単一検索を行います
+   *
+   * @param id 受講生ID
+   * @return 受講生
+   */
   @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(String id);
 
+  /**
+   * 受講生のコース情報の全件検索を行います
+   *
+   * @return 受講生のコース情報（全件）
+   */
   @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCourses();
+  List<StudentsCourses> searchStudentsCourseList();
 
+  /**
+   * 受講生IDに紐づく受講生コース情報を検索します
+   *
+   * @param studentId 受講生ID
+   * @return 受講生IDに紐づく受講生コース情報
+   */
   @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentsCourses> searchStudentCourse(String studentId);
+  List<StudentsCourses> searchStudentCourses(String studentId);
 
   @Insert(
       "INSERT INTO students (name, kana_name, nickname, mail_address, area, age, gender, remark, isDeleted) "
