2c11d8ea599c6963f0eee6ebccdec80b94a93c11
terakura <terakura312@gmail.com>
2024-10-15 11:52:33 +0900

    コードの整形とリファクタリング、StudentsCoursesをStudentCourseに名称変更

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -3,10 +3,10 @@ package raisetech.StudentManagement.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.domain.StudentDetail;
@@ -26,10 +26,10 @@ public class StudentController {
   }
 
   /**
-   * 受講生一覧検索機能
+   * 受講生詳細一覧検索機能
    * 全件検索を行うので、条件指定は行いません
    *
-   * @return 受講生一覧（全件）
+   * @return 受講生詳細一覧（全件）
    */
   @GetMapping("/studentList")
     public List<StudentDetail> getStudentList(){
@@ -37,24 +37,37 @@ public class StudentController {
   }
 
   /**
-   * 受講生検索機能
+   * 受講生詳細検索機能
    * IDに紐づく任意の受講生情報を取得します
    *
    * @param id 受講生ID
-   * @return 受講生
+   * @return 受講生詳細
    */
   @GetMapping("/student/{id}")
   public StudentDetail getStudent(@PathVariable String id){
     return service.searchStudent(id);
   }
 
+  /**
+   * 受講生詳細の新規登録機能
+   *
+   * @param studentDetail 受講生詳細
+   * @return 実行結果
+   */
   @PostMapping("/registerStudent")
-  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail, BindingResult result){
+  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail){
     StudentDetail responseStudentDetail1 = service.registerStudent(studentDetail);
     return ResponseEntity.ok(responseStudentDetail1);
   }
 
-  @PostMapping("/updateStudent")
+  /**
+   * 受講生詳細の更新機能
+   * キャンセルフラグの更新もここで行います（論理削除）
+   *
+   * @param studentDetail 受講生詳細
+   * @return 実行結果
+   */
+  @PutMapping("/updateStudent")
   public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail){
     service.updateStudent(studentDetail);
     return ResponseEntity.ok("更新処理が成功しました。");
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.data.StudentCourse;
 
 /**
  * 受講生テーブルと受講生コース情報テーブルに紐づくRepository
@@ -38,7 +38,7 @@ public interface StudentRepository {
    * @return 受講生のコース情報（全件）
    */
   @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCourseList();
+  List<StudentCourse> searchStudentsCourseList();
 
   /**
    * 受講生IDに紐づく受講生コース情報を検索します
@@ -47,24 +47,44 @@ public interface StudentRepository {
    * @return 受講生IDに紐づく受講生コース情報
    */
   @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentsCourses> searchStudentCourses(String studentId);
+  List<StudentCourse> searchStudentCourseList(String studentId);
 
+  /**
+   * 受講生を新規登録を行います。また、IDに関しては自動採番を行います
+   *
+   * @param student 受講生
+   */
   @Insert(
       "INSERT INTO students (name, kana_name, nickname, mail_address, area, age, gender, remark, isDeleted) "
           + "VALUES (#{name}, #{kanaName}, #{nickname}, #{mailAddress}, #{area}, #{age}, #{gender}, #{remark}, false)")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
 
+  /**
+   * 受講生コース情報の新規登録を行います。また、IDに関しては自動採番を行います
+   *
+   * @param studentCourse 受講生コース情報
+   */
   @Insert("INSERT INTO students_courses(student_id, course_name, starting_date, end_date)"
       + "VALUES (#{studentId}, #{courseName}, #{startingDate}, #{endDate})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
-  void registerStudentsCourses(StudentsCourses studentsCourses);
+  void registerStudentCourse(StudentCourse studentCourse);
 
+  /**
+   * 受講生を更新します
+   *
+   * @param student 受講生
+   */
   @Update(
       "UPDATE students SET name = #{name}, kana_name = #{kanaName}, nickname = #{nickname}, mail_address = #{mailAddress},"
           + "area = #{area}, age = #{age}, gender = #{gender}, remark = #{remark}, isDeleted = #{isDeleted} WHERE id = #{id}")
   void updateStudent(Student student);
 
+  /**
+   * 受講生コース情報のコース名を更新します
+   *
+   * @param studentCourse 受講生コース情報
+   */
   @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id}")
-  void updateStudentsCourses(StudentsCourses studentsCourses);
+  void updateStudentCourse(StudentCourse studentCourse);
 }
